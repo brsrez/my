9e715010f6915c4cc273ab2163c00ebd193a9824
brsrez <3571679@gmail.com>
2025-09-19 20:28:35 +0200

    Hero/h1-bg-words done

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
+import Hero from "./sections/Hero";
+
 const App = () => {
   return (
-    <main>
-      <div className="">
-        <h1>Welcome to my 3D-portfolio!</h1>
-      </div>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 };
 
 export default App;
+  
---
+++ src/constants/index.js
@@ -0,0 +1,34 @@
+export const words = [
+  {
+    text: "Ideas",
+    imgPath: "/public/images/ideas.svg",
+  },
+  {
+    text: "Concepts",
+    imgPath: "/public/images/concepts.svg",
+  },
+  {
+    text: "Designs",
+    imgPath: "/public/images/Designs.svg",
+  },
+  {
+    text: "Code",
+    imgPath: "/public/images/Code.svg",
+  },
+  {
+    text: "Ideas",
+    imgPath: "/public/images/ideas.svg",
+  },
+  {
+    text: "Concepts",
+    imgPath: "/public/images/concepts.svg",
+  },
+  {
+    text: "Designs",
+    imgPath: "/public/images/Designs.svg",
+  },
+  {
+    text: "Code",
+    imgPath: "/public/images/Code.svg",
+  },
+];
